681672015943af9e9cff71fcd5535ed5bd26e45a
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-07-26 18:19:56 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://railanteles.github.io/ColorIMG',
+  base: '/ColorIMG/',
   plugins: [react()]
 });
